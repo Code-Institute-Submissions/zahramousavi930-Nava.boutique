7dada78f2284b898e0e1831450ff2f8721345df9
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 20:30:10 +0330

    create products models

--- main_module/models.py
+++ main_module/models.py
@@ -12,3 +12,23 @@ class Category(models.Model):
 
     def __str__(self):
         return self.categoryy
+
+
+
+
+class Products(models.Model):
+    name=models.CharField(max_length=300)
+    slug=models.CharField(max_length=300)
+    category=models.ForeignKey(Category,on_delete=models.CASCADE,null=True)
+    price=models.IntegerField()
+    size=models.IntegerField()
+    description=models.TextField(max_length=9000)
+    image=models.ImageField(upload_to='products')
+    favorit=models.ManyToManyField(User,null=True,blank=True)
+
+    discount=models.IntegerField(null=True,default=0)
+    discount_price=models.IntegerField(null=True,default=0)
+
+
+    def __str__(self):
+        return self.name
