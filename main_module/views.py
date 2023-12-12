9fcbfd8ed56b848472d26c1bb0adc7c01ab5f3d2
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 20:40:34 +0330

    insert detail from database to home page

--- main_module/views.py
+++ main_module/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from . import models
 # Create your views here.
 
 
 
 class Home_page(TemplateView):
     template_name = 'home_page.html'
+
+
     def get_context_data(self, **kwargs):
 
         context=super(Home_page, self).get_context_data()
-
-
+        context['products']=models.Products.objects.all()[:6]
+        context['category']=models.Category.objects.all()
+        print(self.request.user.id)
         return context
+
+
