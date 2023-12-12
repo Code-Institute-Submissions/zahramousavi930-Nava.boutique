938b98d46ee5c1b4ea13820d6e2997ce3d7d84e1
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 20:29:29 +0330

    create category module

--- main_module/models.py
+++ main_module/models.py
@@ -1,5 +1,14 @@
-
 from django.db import models
+from account_module.models import User
+# Create your models here.
 
 
 
+
+class Category(models.Model):
+    image=models.ImageField(upload_to='category')
+    parent=models.ForeignKey('Category',on_delete=models.CASCADE,null=True,blank=True)
+    categoryy=models.CharField(max_length=300,null=True,blank=True)
+
+    def __str__(self):
+        return self.categoryy
