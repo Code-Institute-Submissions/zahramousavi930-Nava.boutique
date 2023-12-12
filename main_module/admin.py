f2922d077bba1543b8e8dcadb5d8eea8ed5a2f7f
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 20:36:13 +0330

    add models to admin

--- main_module/admin.py
+++ main_module/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+class admin_product(admin.ModelAdmin):
+    list_display = ('name','category','price','discount_price','discount')
+    list_editable = ('price','discount_price','discount')
+
+
+class admin_gift(admin.ModelAdmin):
+    list_display = ('name','category','price','discount_price','discount')
+    list_editable = ('price','discount_price','discount')
+
+admin.site.register(models.Category)
+admin.site.register(models.Products,admin_product)
+admin.site.register(models.News_teller)
+admin.site.register(models.add_comments)
+admin.site.register(models.contact_with_us)
+admin.site.register(models.contact)
+admin.site.register(models.Order)
+admin.site.register(models.OrderDetail)
