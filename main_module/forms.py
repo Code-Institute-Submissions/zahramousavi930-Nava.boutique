da57d9d47dea81d9aa1a2b8ab3a0a70fd3fac667
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 20:37:10 +0330

    create comments and contact us form

---
+++ main_module/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import add_comments ,contact
+
+
+
+class comments(forms.ModelForm):
+    class Meta:
+        model=add_comments
+        fields=('email','text')
+
+class contact_form(forms.ModelForm):
+    class Meta:
+        model= contact
+        fields=('email','name','text',)
