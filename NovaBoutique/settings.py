fefe5e8f39d88af4b2d10eec434c81ab58544c71
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-12 19:10:13 +0000

    diable static

--- NovaBoutique/settings.py
+++ NovaBoutique/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'account_module',
     'main_module',
     'widget_tweaks',
