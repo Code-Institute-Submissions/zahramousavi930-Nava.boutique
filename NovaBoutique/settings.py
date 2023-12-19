765a27700a18a3d0788644666f0917678611171a
zahramousavi930 <zahramousavi930@gmail.com>
2023-12-19 16:10:01 +0330

    test cart

--- NovaBoutique/settings.py
+++ NovaBoutique/settings.py
@@ -103,17 +103,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'NovaBoutique.wsgi.application'
 AUTH_USER_MODEL = 'account_module.User'
 
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default':dj_database_url.parse(os.environ.get('DATABASE_URL'))
+# }
 
 
 # Password validation
