0297e630d4e6842c8951e4894238401fb3848ebc
zahramousavi930 <zahramousavi930@gmail.com>
2024-03-09 17:27:18 +0000

    change settings

--- NovaBoutique/settings.py
+++ NovaBoutique/settings.py
@@ -190,7 +190,6 @@ STRIPE_SECRET_KEY =  STRIPE_SECRET_KEY
 STRIPE_WEBHOOK_SECRET = STRIPE_WEBHOOK_SECRET
 
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
